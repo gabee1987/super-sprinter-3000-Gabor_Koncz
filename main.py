34fddb028219f036d629033bd53c7c1db02bf4e6
gabee1987 <gabee1987@gmail.com>
2017-04-26 10:53:37 +0200

    creating the basic folders and files

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/list')
+def story_list():
+    stories = 
+
+
+
+
+
+
+
+
+
+if__name__ == "__main__":
+    app.run(debug=True)
