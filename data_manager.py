366695e1904f793194a4880fb01e3d0175a12d9c
gabee1987 <gabee1987@gmail.com>
2017-04-28 14:56:08 +0200

    adding stories to file finally works

--- data_manager.py
+++ data_manager.py
@@ -20,7 +20,7 @@ def ID_generator(stories):
     return str(max(ID_numbers) + 1)
 
 
-def open_file(story_ID, filename="database.csv"):
+def open_file(filename="database.csv"):
     """
         Opens the specified file to show its content.
         Reads it content as rows.
@@ -34,77 +34,19 @@ def open_file(story_ID, filename="database.csv"):
         stories = None
 
 
-def write_to_file(stories, filename="databse.csv"):
+def write_to_file(stories, filename="database.csv"):
     """
         Saves data to the specified file.
         Write the entry as rows.
     """
     with open(filename, 'w') as workfile:
         for item in stories:
-            row = ';'.join(item)
+            story = [element.replace("\n", "") for element in item]
+            row = ';'.join(story)
             workfile.write(row + '\n')
 
 
-def add_story(story_ID, filename="database.csv"):
-    """
-        Adds the entries what the user gives.
-    """
-    stories = open_file(story_ID)
-    new_entries = []
-    new_entries.insert(0, ID_generator(stories))
-    form_elements = [
-                    "story_title",
-                    "user_story",
-                    "acceptance_criteria",
-                    "business_value",
-                    "estimation",
-                    "status"
-                    ]
-    for element in form_elements:
-        new_entries.append(request.form[name])
-    stories.append(new_entries)
-    write_to_file(stories)
-    return redirect(url_for('show_story_list'))
 
-
-def delete_story(story_ID, filename="database.csv"):
-    """
-        Removes an entry from the specified file.
-    """
-    stories = open_file()
-    ID = request.form["delete"]
-    for story in stories:
-        if story[0] == ID:
-            stories.remove(story)
-    write_to_file(stories)
-    return redirect(url_for('show_story_list'))
-
-
-def update_story(story_ID, filename="database.csv"):
-    """
-        Update the entry specified by the story_ID.
-    """
-    stories = open_stories()
-    story_ID = request.form['edit']
-    ID = str(story_ID)
-    editing_story = []
-    editing_story.append(ID)
-    form_elements = [
-                    "story_title",
-                    "user_story",
-                    "acceptance_criteria",
-                    "business_value",
-                    "estimation",
-                    "status"
-                    ]
-    for element in form_elements:
-        editing_story.append(request.form[name])
-    stories.append(new_entries)
-    for element in stories:
-        stories.remove(element)
-        stories.insert(0, editing_story)
-    write_to_file(stories)
-    return redirect(url_for('show_story_list'))
 
 
 
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 @app.route('/list', methods=["GET", "POST"])
 def show_story_list():
-    stories = open_file("database.csv")
+    stories = open_file()
     table_header = [
                     "ID",
                     "Story Title",
@@ -29,9 +29,32 @@ def show_story_list():
     return render_template("list.html", stories=stories, table_header=table_header)
 
 
-@app.route("/story", methods=["GET", "POST"])
+@app.route("/story", methods=["GET"])
 def create_story(story_ID=None):
     return render_template("form.html", story_ID=story_ID)
+
+
+@app.route('/story', methods=["POST"])
+def add_story():
+    """
+        Adds the entries what the user gives.
+    """
+    stories = open_file()
+    new_entries = []
+    new_entries.insert(0, ID_generator(stories))
+    form_elements = [
+                    "story_title",
+                    "user_story",
+                    "acceptance_criteria",
+                    "business_value",
+                    "estimation",
+                    "status"
+                    ]
+    for name in form_elements:
+        new_entries.append(request.form[name])
+    stories.append(new_entries)
+    write_to_file(stories)
+    return redirect(url_for('show_story_list'))
 
 
 @app.route("/story/<story_ID>", methods=["GET", "POST"])
@@ -44,6 +67,49 @@ def edit_story(story_ID=None):
         if story[0] == ID:
             story_to_edit.append(story)
     return render_template("form.html", story_ID=story_ID, story_data=story_to_edit)
+
+
+@app.route("/edit_story", methods=["GET", "POST"])
+def update_story(story_ID, filename="database.csv"):
+    """
+        Update the entry specified by the story_ID.
+    """
+    stories = open_stories()
+    story_ID = request.form['edit']
+    ID = str(story_ID)
+    editing_story = []
+    editing_story.append(ID)
+    form_elements = [
+                    "story_title",
+                    "user_story",
+                    "acceptance_criteria",
+                    "business_value",
+                    "estimation",
+                    "status"
+                    ]
+    for element in form_elements:
+        editing_story.append(request.form[name])
+    stories.append(new_entries)
+    for element in stories:
+        stories.remove(element)
+        stories.insert(0, editing_story)
+    write_to_file(stories)
+    return redirect(url_for('show_story_list'))
+
+
+def delete_story(story_ID, filename="database.csv"):
+    """
+        Removes an entry from the specified file.
+    """
+    stories = open_file()
+    ID = request.form["delete"]
+    for story in stories:
+        if story[0] == ID:
+            stories.remove(story)
+    write_to_file(stories)
+    return redirect(url_for('show_story_list'))
+
+
 
 
 
